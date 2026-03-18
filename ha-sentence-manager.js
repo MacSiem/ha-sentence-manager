3f77f08ae92409eb4052150d274481357384ada0
MacSiem <maciek.sieminski@gmail.com>
2026-03-18 10:34:22 +0100

    Fix flickering: increase throttle to 10s + add state hash check

--- ha-sentence-manager.js
+++ ha-sentence-manager.js
@@ -59,7 +59,7 @@ class HASentenceManager extends HTMLElement {
       this._lastRenderTime = now;
       return;
     }
-    if (now - (this._lastRenderTime || 0) < 5000) {
+    if (now - (this._lastRenderTime || 0) < 10000) {
       if (!this._renderScheduled) {
         this._renderScheduled = true;
         setTimeout(() => {
